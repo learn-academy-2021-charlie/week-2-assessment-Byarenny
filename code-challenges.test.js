27c6b94110e369a7eadfbb20136f617bd96b0a2c
byarenny <byarenny@gmail.com>
2021-07-17 12:02:18 -0700

    finished interview questions and coding concepts

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,9 @@
 // ASSESSMENT 2: Coding practical questions with Jest
 
+const { expect } = require("@jest/globals")
+const { it } = require("jest-circus")
+const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // All problems can be solved with concepts covered in class or in the syllabus
 // Pseudo coding is REQUIRED
@@ -27,11 +31,31 @@ var num2 = 0
 var num3 = -7
 // Expected output: "-7 is not divisible by three"
 
-
-
+//PROCESS:
+    //Describe a method that lists the name of the function
+    //it method, nested within the describe code block, that describes what the function does.
+    //an expected method, nested within the it block, calling on the function followed by the .toEqual() matcher that checks the expected output
+describe("evenDiv", () => {
+    it("returns a string that says whether a number is evenly divisible by three", () => {
+        expect(evenDiv(num1)).toEqual(`${num1} is divisible by three`)
+        expect(evenDiv(num2)).toEqual(`${num2} is divisible by three`)
+        expect(evenDiv(num3)).toEqual(`${num3} is not divisible by three`)
+    })
+})
 // b) Create the function that makes the test pass.
-
-
+    //create a function named evenDiv
+    //if num % 3 === 0, it is divisible by three
+    // else if num % 3 != 0, it is not divisible by three
+    const evenDiv = (num) => {
+        if(num % 3 === 0){
+            return `${num} is divisible by three`
+        } else if(num % 3 != 0){
+            return `${num} is not divisible by three`
+        }
+    }
+// console.log(evenDiv(num1))
+// console.log(evenDiv(num2))
+// console.log(evenDiv(num3))
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
@@ -43,11 +67,30 @@ var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
-
+//PROCESS:
+    //describe a method that list the name of the function 
+    //it method, nested within descibe block that describes what the function does
+    //expect method, nested within the it block, calling the function, followed by the .toEqual() matcher that checks the expected output of the function return
+    Describe("capsArray", () => {
+    it("returns an array with all words capitalized", () => {
+        expect(capsArray(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect(capsArray(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+    })
+})
 
 // b) Create the function that makes the test pass.
-
-
+//PROCESS:
+    //create a function named capsArray
+    //create a newArray and use .map()
+    //return newArray
+const capsArray = (array) => {
+    let newArray = array.map(value => {
+        return value[0].toUpperCase() + value.substring(1)
+    })
+    return newArray
+}
+// console.log(capsArray(randomNouns1))
+// console.log(capsArray(randomNouns2))
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
@@ -63,3 +106,4 @@ var vowelTester3 = "challenge"
 
 
 // b) Create the function that makes the test pass.
+
--- coding-concepts.js
+++ coding-concepts.js
@@ -10,19 +10,19 @@
 var cohort = "Charlie 2021"
 // console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: ["Charlie", "2021"]
+// b) Verify and explain: I was partially correct. It did output an array like I had predictied, but each of the characters were separated in quotes. If it was .split(" "), I would have been correct. 
 
 
 // --------------------2) What will this log?
 
-const greeter = (name) => {
-  `Hello, ${name}!`
+const greeter = (string) => {
+  `Hello, ${string}!`
 }
 // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: undefined, there is no return 
+// b) Verify and explain: I was correct, there was nothing to call on the `Hello, ${string}!`
 
 
 // --------------------3) What will this log?
@@ -30,17 +30,17 @@ const greeter = (name) => {
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
 // console.log(multBy2)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [8, 10, 12, 14, 16]
+// b) Verify and explain: I was correct, .map() iterates through an array and in this case it multiplied 2 by each value in the array.
 
 
 // --------------------4) What will this log?
 
 var oddsOnly = [11, 12, 13, 14, 15].filter(value => value % 2 === 0)
-// console.log(oddsOnly)
+console.log(oddsOnly)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [12, 14]
+// b) Verify and explain: I was correct, I thought that it would log even numbers because a value divided by 2 with a remainder of 0 is an even number. 
 
 
 // --------------------5) What will this log?
@@ -55,5 +55,5 @@ class Learn {
 var learnStudent = new Learn
 // console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: {student: "George" , cohort: "Charlie", year: 2021}
+// b) Verify and explain: I was almost fully correct. I forgot to add learn outside of the curly brackets. 
